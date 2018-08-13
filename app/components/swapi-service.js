aecdc89bafa1ec071c49f444456a0ebb394ba190
Isaiah White <isaiahwhite208@gmail.com>
2018-08-13 13:47:00 -0600

    push

--- app/components/swapi-service.js
+++ app/components/swapi-service.js
@@ -1,19 +1,25 @@
-import Person from "../models/Person.js"
+import Person from "../models/Person.js";
+
+
 export default class SwapiService {
+
+
 
   getPeople(draw, drawError) {
     console.log("HELLO FROM SWAPISERVICE")
-    fetch('https://swapi.co/api/starships')
+    fetch('https://swapi.co/api/people')
       .then(res => res.json())
       .then(res => {
-        return res.results.map(rawPerson => {
+        let myPeople = res.results.map(rawPerson => {
           return new Person(rawPerson)
         })
+        draw(myPeople)
       })
       .catch(drawError)
 
     console.log("HERE I AM")
   }
+
 
   getStarships(draw, drawError) {
     console.log("HELLO FROM SWAPISERVICE")
