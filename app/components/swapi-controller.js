9fad59a94db415b71132eaaff1e3186034d48dac
Isaiah White <isaiahwhite208@gmail.com>
2018-08-13 14:42:34 -0600

    push

--- app/components/swapi-controller.js
+++ app/components/swapi-controller.js
@@ -1,16 +1,77 @@
 import SwapiService from "./swapi-service.js";
+import Vehicle from "../models/Vehicle.js";
+
+// Private
 
 const swapiService = new SwapiService
 let app = document.getElementById('app')
 
 function draw(data) {
   console.log(data)
-  app.innerHTML = `
+  app.innerHTML =
+    `
+  <div id="error"></div>
+  <button onclick="app.controllers.swapi.getPeople()">
+  Get People
+  </button>
+  <div id="people"></div>`;
+  `
+  <div id="error"></div>
+  <button onclick="app.controllers.swapi.getPlanet()">
+  Get Planets
+  </button>
+  <div id="planets"></div>`;
+  `
   <div id="error"></div>
   <button onclick="app.controllers.swapi.getStarships()">
   Get Starships
   </button>
-  <div id="starships"></div>`
+  <div id="starships"></div>`;
+  `
+  <div id="error"></div>
+  <button onclick="app.controllers.swapi.getVehicle()">
+  Get Vehicles
+  </button>
+  <div id="vehicles"></div>`
+}
+
+function drawPeople(data) {
+  let peopleElem = document.getElementById('people')
+  let template = ''
+  data.results.forEach(people => {
+    template += `<div>
+    ${people.name}
+    </div>`
+  })
+
+  peopleElem.innerHTML = template
+
+}
+
+function drawPlanets(data) {
+  let planetsElem = document.getElementById('planets')
+  let template = ''
+  data.results.forEach(planet => {
+    template += `<div>
+    ${planet.name}
+    </div>`
+  })
+
+  planetsElem.innerHTML = template
+
+}
+
+function drawVehicles(data) {
+  let vehiclesElem = document.getElementById('vehicles')
+  let template = ''
+  data.results.forEach(vehicle => {
+    template += `<div>
+    ${vehicle.name}
+    </div>`
+  })
+
+  vehiclesElem.innerHTML = template
+
 }
 
 function drawStarships(data) {
@@ -31,6 +92,7 @@ function drawError(error) {
   document.getElementById('error').innerHTML = error.message
 }
 
+// public
 
 export default class SwapiController {
   constructor() {
